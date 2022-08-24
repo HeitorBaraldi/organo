1db02fbab46f3f711d7337f4697cab38cbb425b7
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-08-24 13:19:14 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,17 @@
+import './Banner.css'
+
+export const Banner =() =>{
+    //JSX 
+    return (
+        <header className='banner'>
+        <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+
+}
+// Exemplo de vários Banner
+export const Banny = () => {
+    return (
+        <h1>OLÁ, COLOCAR O BANNER SUBSTITUTO AQUI !!!!!</h1>
+    )
+}
